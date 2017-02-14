3b757ccfc494f0fc732d1f69927196b7dde09c58
swethagowda <thashswe@gmail.com>
2017-02-14 14:55:33 -0800

    Changed names in loginTest

--- src/test/java/com/swe/SalesApp/LoginTest.java
+++ src/test/java/com/swe/SalesApp/LoginTest.java
@@ -1,10 +1,24 @@
 package com.swe.SalesApp;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class LoginTest {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Sales application");
 		System.out.println("welcome to sales ");
+		int sum=0;
+		List<Integer> list=new ArrayList<Integer>(Arrays.asList(2,4,5,1));
+		for(int num=0;num<=list.size();num++){
+			
+				sum=sum+num;
+			System.out.println(sum);
+			}		
+		
 	}
 
 }
